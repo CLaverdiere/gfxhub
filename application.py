e513f28982d2872e46f7530bb310192944c6b9fa
Chris Laverdiere <cmlaverdiere@gmail.com>
2014-06-27 23:51:59 -0400

    Auto file detection in graphics directory.

--- application.py
+++ application.py
@@ -1,18 +1,24 @@
+import os
 from flask import Flask, url_for, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def gfxhub(name=None):
     return render_template('application.html', name=name)
 
-@app.route('/g/')
-@app.route('/g/<n>')
-def show_graphic(n=0):
-    return render_template('graphics.html', n=n)
-
 @app.route('/about')
-def about(n):
+def about():
     return "gfxhub is a place to share graphics."
+
+@app.route('/g/')
+def show_graphic_list(pics=[]):
+    pics = sorted(filter(lambda f: f.endswith(".png"), os.listdir("static/raytraced_pics/")))
+    return render_template('graphics_list.html', pics=pics)
+
+@app.route('/g/<pic>')
+def show_graphic(pic=None):
+    return render_template('graphics.html', pic=pic)
 
 @app.errorhandler(404)
 def page_not_found(error):
@@ -20,5 +26,3 @@ def page_not_found(error):
 
 if __name__ == "__main__":
     app.run(debug=True)
-    for i in range(1, 12):
-        url_for('static', filename='raytraced_pics/s{}.png'.format(i))
